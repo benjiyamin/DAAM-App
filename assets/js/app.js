12d5a4c16855ac0975aadf6891ade05e4d20249c
Ben Miller <mail@benkmiller.com>
2019-04-27 11:27:47 -0400

    Restaurant card info

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,29 @@
+function starsHtml(rating, maxRating = 5) {
+  let html = ''
+  let flooredRating = Math.floor(rating)
+  for (let i = 0; i < maxRating; i++) {
+    if (i <= flooredRating) {
+      html += '<i class="fas fa-star"></i>'
+    } else {
+      html += '<i class="fas fa-star text-muted"></i>'
+    }
+    //if (flooredRating - rating === 0.5) {
+    //  html += '<i class="fas fa-star text-muted"></i>'
+    //}
+
+    //<i class="fas fa-star-half"></i>
+  }
+  return html
+}
+
+function categoryString(categories) {
+  let string = ''
+  categories.forEach(category => {
+    string += ', ' + category.title
+  });
+  return string
+}
+
 function Application() {
   let self = this
   this.zipCode = undefined
@@ -5,7 +31,7 @@ function Application() {
   this.startTime = undefined
   this.endTime = undefined
   this.theaters = [1, 2, 3]
-  this.restaurants = [1, 2, 3]
+  this.restaurants = []
   let yelpApi = 'cqri1UB-gnIXTN3mPx4GAi4vRAEpWc7KDG3n3HS2uC6nNBaG45cH3_8Wi7aPN1v5GHjHihhJ5MVWUHC1f8N1muxqS8Muqtp9FdqtWe3FVvY3CI0uDVpshApC41nEXHYx'
 
   this.getTheaters = function (zipcode) {}
@@ -35,42 +61,27 @@ function Application() {
     })
   }
 
-  this.getRestaurants = function (address) {
-    let queryUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&radius=5000&sort_by=distance&limit=5&location=" + address;
-
-    $.ajax({
-      url: queryUrl,
-      headers: {
-        'Authorization': 'Bearer ' + yelpApi,
-      },
-      method: 'GET',
-      dataType: 'json',
-      success: function (data) {
-        console.log(data)
-      }
-    });
-  }
-
   this.renderRestaurants = function () {
+
     let $restaurantResults = $('#restaurantResults')
     $restaurantResults.empty()
     this.restaurants.forEach(restaurant => {
       let name = $('<h5>')
         .addClass('card-title')
-        .text("Trevor's Tacos")
+        .text(restaurant.name)
       let rating = $('<p>')
         .addClass('card-text')
-        .html('<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star text-muted"></i><i class="fas fa-star text-muted"></i></p>')
-      let cost = $('<p>')
+        .html(starsHtml(restaurant.rating))
+      let price = $('<p>')
         .addClass('card-text')
-        .text('$$$$')
+        .text(restaurant.price + ' | ' + categoryString(restaurant.categories))
       let main = $('<div>')
         .addClass('col-4')
-        .append(name, rating, cost)
+        .append(name, rating, price)
       let phone = $('<p>')
-        .text('(407) 201-3583')
+        .text(restaurant.phone)
       let address = $('<p>')
-        .text('123 Burrito Rd')
+        .text(restaurant.location.address1)
       let contact = $('<div>')
         .addClass('col-8 text-right card-text')
         .append(phone, address)
@@ -81,6 +92,22 @@ function Application() {
         .addClass('card mt-4')
         .append(cardBody)
       $restaurantResults.append(card)
+    })
+  }
+
+  this.loadRestaurants = function (address) {
+    let queryUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&sort_by=distance&limit=5&location=" + address;
+    $.ajax({
+      url: queryUrl,
+      headers: {
+        'Authorization': 'Bearer ' + yelpApi,
+      },
+      method: 'GET',
+      dataType: 'json',
+    }).done(function(data) {
+      self.restaurants = data.businesses
+      self.renderRestaurants()
+      console.log(data)
     })
   }
 }
