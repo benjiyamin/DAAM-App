4f7921ac64ed5cc0b6e29fedada12f18c52588ab
Ben Miller <mail@benkmiller.com>
2019-04-27 23:05:56 -0400

    Card rendering logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,14 @@
 function starsHtml(rating, maxRating = 5) {
   let html = ''
-  let flooredRating = Math.floor(rating)
+  //let flooredRating = Math.floor(rating)
   for (let i = 0; i < maxRating; i++) {
-    if (i <= flooredRating) {
+    if (rating - i >= 1) {
       html += '<i class="fas fa-star"></i>'
+    } else if (rating - i > 0) {
+      html += '<i class="fas fa-star-half-alt"></i>'
     } else {
-      html += '<i class="fas fa-star text-muted"></i>'
+      html += '<i class="far fa-star"></i>'
     }
-    //if (flooredRating - rating === 0.5) {
-    //  html += '<i class="fas fa-star text-muted"></i>'
-    //}
-
-    //<i class="fas fa-star-half"></i>
   }
   return html
 }
@@ -30,42 +27,161 @@ function Application() {
   this.date = undefined
   this.startTime = undefined
   this.endTime = undefined
-  this.theaters = [1, 2, 3]
+  this.movies = []
+  this.movie = undefined
+  //this.theater = undefined
+  this.theaters = []
+  this.showtime = undefined
   this.restaurants = []
+  this.restaurant = undefined
   let yelpApi = 'cqri1UB-gnIXTN3mPx4GAi4vRAEpWc7KDG3n3HS2uC6nNBaG45cH3_8Wi7aPN1v5GHjHihhJ5MVWUHC1f8N1muxqS8Muqtp9FdqtWe3FVvY3CI0uDVpshApC41nEXHYx'
+  let tmsApi = 'kq8d5zhpr87bvbz6cufpdgqt'
+  let gmapsApi = 'AIzaSyDWFMiZeZwNNAdJUZsfMZ7edVnxgLOSfDs'
+  let omdbApi = 'bd02b758'
 
-  this.getTheaters = function (zipcode) {}
+  this.renderMovies = function () {
+    let $movieResults = $('#movieResults')
+    $movieResults.empty()
+    this.movies.forEach(movie => {
+      let cardImg = $('<img>')
+        .addClass('card-img')
+      let queryUrl = "https://www.omdbapi.com/?t=" + movie.title + "&apikey=" + omdbApi;
+      $.ajax({
+        url: queryUrl,
+        method: 'GET',
+        dataType: 'json',
+      }).done(function (data) {
+        console.log(data.Poster)
+        if (/(jpg|gif|png|JPG|GIF|PNG|JPEG|jpeg)$/.test(data.Poster)){ // image url as input
+          cardImg.attr('src', data.Poster)
+        } else {
+          cardImg.attr('src', 'https://via.placeholder.com/200x300')
+        }
+      })
+      let poster = $('<div>')
+        .addClass('col-md-4')
+        .append(cardImg)
+      let name = $('<h5>')
+        .addClass('card-title')
+        .text(movie.title + ' (' + movie.releaseYear + ')')
+      let rating = $('<p>')
+        .addClass('card-text')
+        .text(movie.runTime)
+      let cardBody = $('<div>')
+        .addClass('card-body')
+        .append(name, rating)
+      let info = $('<div>')
+        .addClass('col-md-8')
+        .append(cardBody)
+      let row = $('<div>')
+        .addClass('row no-gutters')
+        .append(poster, info)
+      let card = $('<div>')
+        .addClass('card movie-card')
+        .append(row)
+        .click(function () {
+          let nextTabId = $(this)
+            .closest('div.tab-pane')
+            .next()
+            .attr('id')
+          let $nextTab = $('#myTab a[href="#' + nextTabId + '"]')
+          let $movieNext = $('#movieNext')
+          if ($(this).hasClass('border-primary')) {
+            // None selected
+            $movieNext.prop('disabled', true)
+            $nextTab.addClass('disabled')
+          } else {
+            // Card selected
+            $('.movie-card').removeClass('border-primary')
+            $movieNext.prop('disabled', false)
+            $nextTab.removeClass('disabled')
+            self.movie = movie
+          }
+          $(this).toggleClass('border-primary')
+        })
+      let col = $('<div>')
+        .addClass('col col-md-6 col-lg-4 mt-4')
+        .append(card)
+      $movieResults.append(col)
+    })
+  }
+
+  this.loadMovies = function (zipcode) {
+    let queryUrl = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-04-27&zip=' + zipcode + '&api_key=' + tmsApi
+    $.ajax({
+      url: queryUrl,
+      method: 'GET',
+      dataType: 'json',
+    }).done(function (data) {
+      self.movies = data
+      self.renderMovies()
+      console.log(data)
+    })
+  }
 
   this.renderTheaters = function () {
     let $theaterResults = $('#theaterResults')
     $theaterResults.empty()
-    this.theaters.forEach(theater => {
-      let name = $('<h5>')
-        .addClass('card-title')
-        .text("Molly's Movies") // theater.name
-      let address = $('<p>')
-        .addClass('card-text')
-        .text('123 Movie Dr') // theater.address
-      let location = $('<div>')
-        .addClass('col-4')
-        .append(name, address)
-      let times = $('<div>')
-        .addClass('col-8 text-right')
-      let cardBody = $('<div>')
-        .addClass('card-body row')
-        .append(location, times)
-      let card = $('<div>')
-        .addClass('card mt-4')
-        .append(cardBody)
-      $theaterResults.append(card)
-    })
+
+    for (const theaterName in this.theaters) {
+      if (this.theaters.hasOwnProperty(theaterName)) {
+        const showtimes = this.theaters[theaterName];
+        let name = $('<h5>')
+          .addClass('card-title')
+          .text(theaterName)
+        let address = $('<p>')
+          .addClass('card-text')
+          .text('123 Movie Dr') // theater.address
+        let location = $('<div>')
+          .addClass('col-4')
+          .append(name, address)
+        let times = $('<div>')
+          .addClass('col-8 text-right')
+        showtimes.forEach(showtime => {
+          let time = moment(showtime.dateTime).format('h:mm a')
+          let button = $('<button>')
+            //.attr('href', showtime.ticketURI)
+            //.attr('data-toggle', 'button')
+            .addClass('btn btn-lg btn-info btn-showtime')
+            .text(time)
+            .click(function () {
+              let nextTabId = $(this)
+                .closest('div.tab-pane')
+                .next()
+                .attr('id')
+              let $nextTab = $('#myTab a[href="#' + nextTabId + '"]')
+              let $theaterNext = $('#theaterNext')
+              if ($(this).hasClass('active')) {
+                // None selected
+                $theaterNext.prop('disabled', true)
+                $nextTab.addClass('disabled')
+              } else {
+                // Button selected
+                $('.btn-showtime').removeClass('active')
+                $theaterNext.prop('disabled', false)
+                $nextTab.removeClass('disabled')
+                self.showtime = showtime
+              }
+              $(this).toggleClass('active')
+            })
+          times.append(button)
+        });
+        let cardBody = $('<div>')
+          .addClass('card-body row')
+          .append(location, times)
+        let card = $('<div>')
+          .addClass('card mt-4 theater-card')
+          .append(cardBody)
+        $theaterResults.append(card)
+      }
+    }
   }
 
   this.renderRestaurants = function () {
-
     let $restaurantResults = $('#restaurantResults')
     $restaurantResults.empty()
     this.restaurants.forEach(restaurant => {
+      //console.log(restaurant)
       let name = $('<h5>')
         .addClass('card-title')
         .text(restaurant.name)
@@ -89,14 +205,27 @@ function Application() {
         .addClass('card-body row')
         .append(main, contact)
       let card = $('<div>')
-        .addClass('card mt-4')
+        .addClass('card mt-4 restaurant-card')
         .append(cardBody)
+        .click(function () {
+          let $restaurantNext = $('#restaurantNext')
+          if ($(this).hasClass('border-primary')) {
+            // None selected
+            $restaurantNext.prop('disabled', true)
+          } else {
+            // Card selected
+            $('.restaurant-card').removeClass('border-primary')
+            $restaurantNext.prop('disabled', false)
+            self.restaurant = restaurant
+          }
+          $(this).toggleClass('border-primary')
+        })
       $restaurantResults.append(card)
     })
   }
 
-  this.loadRestaurants = function (address) {
-    let queryUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&sort_by=distance&limit=3&location=" + address;
+  this.loadRestaurants = function (location) {
+    let queryUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&sort_by=distance&limit=3&location=" + location;
     $.ajax({
       url: queryUrl,
       headers: {
@@ -126,4 +255,20 @@ function Application() {
       .attr('id')
     $('#myTab a[href="#' + tabId + '"]').tab('show')
   })
+
+  $('#movieNext').on('click', function () {
+    self.theaters = _.groupBy(self.movie.showtimes, function (showtime) {
+      return showtime.theatre.name
+    })
+    $('#movieTitle').text(self.movie.title)
+    $('#theaterZipcode').text(self.zipCode)
+    self.renderTheaters()
+  })
+
+  $('#theaterNext').on('click', function () {
+    //console.log(self.showtime)
+    self.loadRestaurants(self.showtime.theatre.name)
+    $('#theaterName').text(self.showtime.theatre.name)
+  })
+
 }
